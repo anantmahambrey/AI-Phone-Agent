690c1b74fde425cb8e60ab0afcc7542c037cbf2b
anantmahambrey <anantmahambrey@gmail.com>
2025-01-15 01:31:27 +0530

    added requirements.txt

--- audio_transcription/settings.py
+++ audio_transcription/settings.py
@@ -28,7 +28,7 @@ GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anant1911.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
